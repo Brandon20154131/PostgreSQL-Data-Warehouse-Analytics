f72e1a1696f634207f3c8b13f511ec361613baa8
Brandon20154131 <154557913+Brandon20154131@users.noreply.github.com>
2025-10-12 09:37:35 +0100

    Delete Scripts directory

--- Scripts/init_database.sql
+++
@@ -1,7 +0,0 @@
--- Connect to the default database (postgres) before running this part
-
--- Drop the database if it exists
-DROP DATABASE IF EXISTS "DataWarehouse";
-
--- Create the new database
-CREATE DATABASE "DataWarehouse";
