afeae761f3270487b7ee3d9305b3c5422aba58d1
Brandon20154131 <154557913+Brandon20154131@users.noreply.github.com>
2025-10-12 09:48:41 +0100

    Update ddl_bronze.sql

--- data warehouse/scripts/bronze/ddl_bronze.sql
+++ data warehouse/scripts/bronze/ddl_bronze.sql
@@ -1,9 +1,13 @@
--- ==================================================================================================
--- DDL Script: Create Bronze Tables 
--- ==================================================================================================
--- Script Purpose:
--- This script creates tables in the 'bronze' schema, dropping existing tables if they already exist.
--- ==================================================================================================
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
 -- CRM Customer Info
 DROP TABLE IF EXISTS bronze.crm_cust_info;
@@ -27,8 +31,8 @@ CREATE TABLE bronze.crm_prd_info (
     prd_nm       VARCHAR(50),
     prd_cost     INTEGER,
     prd_line     VARCHAR(50),
-    prd_start_dt DATE,
-    prd_end_dt   DATE
+    prd_start_dt TIMESTAMP,
+    prd_end_dt   TIMESTAMP
 );
 
 -- CRM Sales Details
